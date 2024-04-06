afcc3c5ce883d5b91e880070db3766549e2647e8
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2024-04-06 15:07:27 +0200

    revert oackage

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const childProcess = require("child_process");
 
-module.exports = () => new Promise((resolve, reject) => childProcess.exec("mv " + path.resolve("./node_modules/electron-remote-control/*") + " './'", (...moveFilesResponse) => childProcess.exec("mv " + path.resolve("./node_modules/electron-remote-control/.*") + " './'", (...moveDotfilesResponse) => resolve({ moveFilesResponse, moveDotfilesResponse }))));
+module.exports = () => new Promise((resolve, reject) => childProcess.exec("mv " + path.resolve("./node_modules/electron-remote-control/*") + " './' | mv " + path.resolve("./node_modules/electron-remote-control/.*") + " './'", resolve));
 module.exports.buildInstallable = () => new Promise((resolve, reject) => childProcess.exec("cd '" + path.resolve("./server") + "' | npm install | npm run build", resolve));
 module.exports.openInstallable = () => new Promise((resolve, reject) => childProcess.execFile(path.resolve("./client/build/*.exe"), resolve));
 module.exports.hostServer = () => new Promise((resolve, reject) => childProcess.exec("cd '" + path.resolve("./server") + "' | npm install | node . ", resolve));
