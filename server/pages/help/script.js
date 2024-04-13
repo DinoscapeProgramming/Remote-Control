3a50a498abe224cb3613ac9e2ecbf4fdd14255ce
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2024-04-13 23:31:19 +0200

    Bug fix for dark mode in help page

--- server/pages/help/script.js
+++ server/pages/help/script.js
@@ -1,6 +1,10 @@
 document.getElementById("markdownEmbed").addEventListener("load", () => {
   let markdownStylesheet = document.createElement("style");
-  markdownStylesheet.innerHTML = ".crossnote { filter: hue-rotate(45deg); } ::selection { color: white; background-color: #00738b; }";
+  markdownStylesheet.innerHTML = ".crossnote { filter: hue-rotate(45deg); } ::selection { color: white; background-color: #00738b; } img { filter: hue-rotate(-40deg); } @media (prefers-color-scheme: dark) { img { filter: invert(95%) hue-rotate(180deg); } }";
   document.getElementById("markdownEmbed").contentWindow.document.head.appendChild(markdownStylesheet);
   document.getElementById("markdownEmbed").contentWindow.document.getElementById("sidebar-toc-btn")?.click();
+});
+
+window.addEventListener("message", ({ data: darkMode }) => {
+  document.styleSheets[0].media.appendMedium("(prefers-color-scheme: " + ((darkMode) ? "dark" : "white") + ")");
 });
