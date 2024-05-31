67e05b676115eb46d72f01bbe5cca2971e9ae378
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2024-05-31 15:34:17 +0200

    Bug fix

--- server/apps/code/e147a27e.js
+++ server/apps/code/e147a27e.js
@@ -9,28 +9,6 @@ localStorage.setItem("translucenceThemeId", createAppStartupCodeFile(() => {
   document.getElementById("pageEmbed").style.opacity = "0.85";
   document.getElementById("pageEmbed").style.borderTopLeftRadius = "0";
   document.getElementById("pageEmbed").style.borderBottomLeftRadius = "0";
-  document.getElementById("pageEmbed").contentWindow.document.body.style.color = "white";
-  document.getElementById("pageEmbed").contentWindow.document.body.children[0].children[0].style.color = "white";
-  if (document.getElementById("pageEmbed").src.endsWith("/scripts/index.html")) {
-    document.getElementById("pageEmbed").contentWindow.document.body.children[2].children[0].children[1].style.color = "white";
-    document.getElementById("pageEmbed").contentWindow.document.getElementById("scriptEditor").style.filter = "none";
-    document.getElementById("pageEmbed").contentWindow.document.getElementById("scriptEditor").style.width = "calc(100% + 2px)";
-    document.getElementById("pageEmbed").contentWindow.document.getElementById("scriptEditor").style.marginLeft = "-1px";
-    document.getElementById("pageEmbed").contentWindow.document.getElementById("scriptEditor").style.marginBottom = "-1px";
-  };
-  if (document.getElementById("pageEmbed").src.endsWith("/debug/index.html")) {
-    document.getElementById("pageEmbed").contentWindow.document.getElementById("debugLogInput").style.color = "white";
-    document.getElementById("pageEmbed").contentWindow.document.getElementById("debugCodeExecutionInput").style.borderTopLeftRadius = "2.5px";
-    document.getElementById("pageEmbed").contentWindow.document.getElementById("debugCodeExecutionInput").style.borderBottomLeftRadius = "2.5px";
-  };
-  if (document.getElementById("pageEmbed").src.endsWith("/settings/index.html")) {
-    document.getElementById("pageEmbed").contentWindow.document.body.children[2].children[0].style.display = "none";
-    document.getElementById("pageEmbed").contentWindow.document.body.children[2].children[1].style.display = "none";
-  };
-  if (document.getElementById("pageEmbed").src.endsWith("/help/index.html")) {
-    document.getElementById("pageEmbed").contentWindow.document.getElementById("markdownEmbed").style.filter = "invert(95%) hue-rotate(180deg)";
-    document.getElementById("pageEmbed").contentWindow.document.getElementById("markdownEmbed").src = ((Object.keys(JSON.parse(fs.readFileSync(path.join(process.resourcesPath, "customServer.json"), "utf8"))).length) ? (JSON.parse(fs.readFileSync(path.join(process.resourcesPath, "customServer.json"), "utf8")).resourceProtocol + "//" + JSON.parse(fs.readFileSync(path.join(process.resourcesPath, "customServer.json"), "utf8")).resourceHostname + ((JSON.parse(fs.readFileSync(path.join(process.resourcesPath, "customServer.json"), "utf8")).resourcePort) ? (":" + JSON.parse(fs.readFileSync(path.join(process.resourcesPath, "customServer.json"), "utf8")).resourcePort) : "")) : (process.env.DEFAULT_RESOURCE_SERVER_PROTOCOL + "//" + process.env.DEFAULT_RESOURCE_SERVER_HOSTNAME + ((process.env.DEFAULT_RESOURCE_SERVER_PORT) ? (":" + process.env.DEFAULT_RESOURCE_SERVER_PORT) : ""))) + "/help/?darkMode=true";
-  };
   if ((JSON.parse(localStorage.getItem("settings")) || {}).darkMode) {
     localStorage.setItem("settings", JSON.stringify({
       ...JSON.parse(localStorage.getItem("settings")) || {},
@@ -66,3 +44,26 @@ localStorage.setItem("translucenceThemeId", createAppStartupCodeFile(() => {
     };
   });
 }));
+
+document.getElementById("pageEmbed").contentWindow.document.body.style.color = "white";
+  document.getElementById("pageEmbed").contentWindow.document.body.children[0].children[0].style.color = "white";
+  if (document.getElementById("pageEmbed").src.endsWith("/scripts/index.html")) {
+    document.getElementById("pageEmbed").contentWindow.document.body.children[2].children[0].children[1].style.color = "white";
+    document.getElementById("pageEmbed").contentWindow.document.getElementById("scriptEditor").style.filter = "none";
+    document.getElementById("pageEmbed").contentWindow.document.getElementById("scriptEditor").style.width = "calc(100% + 2px)";
+    document.getElementById("pageEmbed").contentWindow.document.getElementById("scriptEditor").style.marginLeft = "-1px";
+    document.getElementById("pageEmbed").contentWindow.document.getElementById("scriptEditor").style.marginBottom = "-1px";
+  };
+  if (document.getElementById("pageEmbed").src.endsWith("/debug/index.html")) {
+    document.getElementById("pageEmbed").contentWindow.document.getElementById("debugLogInput").style.color = "white";
+    document.getElementById("pageEmbed").contentWindow.document.getElementById("debugCodeExecutionInput").style.borderTopLeftRadius = "2.5px";
+    document.getElementById("pageEmbed").contentWindow.document.getElementById("debugCodeExecutionInput").style.borderBottomLeftRadius = "2.5px";
+  };
+  if (document.getElementById("pageEmbed").src.endsWith("/settings/index.html")) {
+    document.getElementById("pageEmbed").contentWindow.document.body.children[2].children[0].style.display = "none";
+    document.getElementById("pageEmbed").contentWindow.document.body.children[2].children[1].style.display = "none";
+  };
+  if (document.getElementById("pageEmbed").src.endsWith("/help/index.html")) {
+    document.getElementById("pageEmbed").contentWindow.document.getElementById("markdownEmbed").style.filter = "invert(95%) hue-rotate(180deg)";
+    document.getElementById("pageEmbed").contentWindow.document.getElementById("markdownEmbed").src = ((Object.keys(JSON.parse(fs.readFileSync(path.join(process.resourcesPath, "customServer.json"), "utf8"))).length) ? (JSON.parse(fs.readFileSync(path.join(process.resourcesPath, "customServer.json"), "utf8")).resourceProtocol + "//" + JSON.parse(fs.readFileSync(path.join(process.resourcesPath, "customServer.json"), "utf8")).resourceHostname + ((JSON.parse(fs.readFileSync(path.join(process.resourcesPath, "customServer.json"), "utf8")).resourcePort) ? (":" + JSON.parse(fs.readFileSync(path.join(process.resourcesPath, "customServer.json"), "utf8")).resourcePort) : "")) : (process.env.DEFAULT_RESOURCE_SERVER_PROTOCOL + "//" + process.env.DEFAULT_RESOURCE_SERVER_HOSTNAME + ((process.env.DEFAULT_RESOURCE_SERVER_PORT) ? (":" + process.env.DEFAULT_RESOURCE_SERVER_PORT) : ""))) + "/help/?darkMode=true";
+  };
