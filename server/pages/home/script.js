d40a8995d5c5e1842a140530a83501f52f241e98
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2024-11-01 17:06:10 +0100

    Service Worker blocks main thread

--- server/pages/home/script.js
+++ server/pages/home/script.js
@@ -18,9 +18,9 @@ document.getElementById("newsletterButton").addEventListener("click", () => {
   };
 });
 
-if (!["iPad Simulator", "iPhone Simulator", "iPod Simulator", "iPad", "iPhone", "iPod"].includes(navigator.platform) && !navigator.userAgent.includes("Mac") && !("ontouchend" in document)) {
+/*if (!["iPad Simulator", "iPhone Simulator", "iPod Simulator", "iPad", "iPhone", "iPod"].includes(navigator.platform) && !navigator.userAgent.includes("Mac") && !("ontouchend" in document)) {
   let serviceWorkerRegistration = document.createElement("script");
   serviceWorkerRegistration.setAttribute("defer", "");
   serviceWorkerRegistration.setAttribute("src", "/pages/serviceWorker.js");
   document.head.appendChild(serviceWorkerRegistration);
-};
+};*/
