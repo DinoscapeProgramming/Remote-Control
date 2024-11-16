54359d6029539d12081924a71ed8df42a60b22a7
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2024-11-16 13:51:12 +0100

    Cache database

--- server/database.js
+++ server/database.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
+let databaseCache = {};
 
 module.exports = {
   readDatabase: (databaseName) => new Promise((resolve, reject) => {
+    if (databaseCache[databaseName] && ((Date.now() - databaseCache[databaseName].timestamp) < 86400000)) return resolve({ err: null, [databaseName]: databaseCache[databaseName].data });
     fetch("https://api.jsonbin.io/v3/b/" + process.env["DATABASE_" + databaseName.toUpperCase()] + "/latest", {
       method: "GET",
       headers: {
@@ -12,11 +14,19 @@ module.exports = {
     .then((response) => response.json())
     .then(({ message, record }) => {
       if (message) reject({ err: message, [databaseName]: null });
+      databaseCache[databaseName] = {
+        data: record,
+        timestamp: Date.now()
+      };
       resolve({ err: null, [databaseName]: record });
     })
     .catch((err) => reject({ err, [databaseName]: null }));
   }),
   updateDatabase: (databaseName, databaseContent) => new Promise((resolve, reject) => {
+    if ((Date.now() - databaseCache[databaseName].timestamp) < 86400000) return ((cache[databaseCache] = {
+      data: databaseContent,
+      timestamp: Date.now()
+    }), resolve({ err: null }));
     fetch("https://api.jsonbin.io/v3/b/" + process.env["DATABASE_" + databaseName.toUpperCase()], {
       method: "PUT",
       headers: {
@@ -28,6 +38,10 @@ module.exports = {
     .then((response) => response.json())
     .then(({ message }) => {
       if (message) reject({ err: message });
+      databaseCache[databaseName] = {
+        data: record,
+        timestamp: Date.now()
+      };
       resolve({ err: null });
     })
     .catch((err) => reject({ err, [databaseName]: null }));
