52e2fe826fcc595113fa0d874fd4a41b69a9dcdb
DinoscapeProgramming <95239392+DinoscapeProgramming@users.noreply.github.com>
2024-11-29 12:53:24 +0100

    Bug fix

--- client/pages/scripts/script.js
+++ client/pages/scripts/script.js
@@ -16,7 +16,7 @@ scriptEditor.on("change", () => {
   scriptEditor.renderer.updateFull();
 })).observe(document.getElementById("scriptEditor"));
 window.process = {
-  env: fs.readFileSync(path.join(parent.process.resourcesPath, "app.asar/.env"), "utf8").split("\r\n").filter((line) => !line.startsWith("#") && (line.split("=").length > 1)).map((line) => line.split("#")[0].split("=")).reduce((data, accumulator) => ({
+  env: fs.readFileSync(path.join(parent.process.resourcesPath, "app.asar/.env"), "utf8").split("\n").filter((line) => !line.startsWith("#") && (line.split("=").length > 1)).map((line) => line.split("\r")[0].split("#")[0].split("=")).reduce((data, accumulator) => ({
     ...data,
     ...{
       [accumulator[0]]: JSON.parse(accumulator[1])
